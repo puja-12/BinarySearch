bda8eec20fe87db0e0f49bdcb29b3cff24dec3c4
puja <pujarana1796@gmail.com>
2022-05-13 11:21:40 +0530

    puja added first commit

---
+++ BinarySearchTree/BST.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+   
+        public class BST<T> where T : IComparable<T>
+        {
+            public T NodeData { get; set; }
+            public BST<T> LeftTree { get; set; }
+            public BST<T> RightTree { get; set; }
+            public BST(T nodeData)
+            {
+                this.NodeData = nodeData;
+                this.LeftTree = LeftTree;
+                this.RightTree = RightTree;
+            }
+            int leftCount = 0, rightCount = 0;
+            bool result = false;
+            public void Insert(T item)
+            {
+                T currentNodeValue = this.NodeData;
+                if (currentNodeValue.CompareTo(item) > 0)
+                {
+                    if (this.LeftTree == null)
+                        this.LeftTree = new BST<T>(item);
+                    else
+                        this.LeftTree.Insert(item);
+                }
+                else
+                {
+                    if (this.RightTree == null)
+                        this.RightTree = new BST<T>(item);
+                    else
+                        this.RightTree.Insert(item);
+                }
+            }
+            public void Display()
+            {
+                if (this.LeftTree != null)
+                {
+                    this.leftCount++;
+                    this.LeftTree.Display();
+
+                }
+                Console.WriteLine(this.NodeData.ToString());
+                if (this.RightTree != null)
+                {
+                    this.rightCount++;
+                    this.RightTree.Display();
+                }
+            }
+
+        }
+    }
+
+
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Welcome to binary tree search!");
+using System;
+namespace BinarySearchTree
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to binary tree search!");
+
+            BST<int> bst = new(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.Display();
+
+        }
+    }
+}
+
