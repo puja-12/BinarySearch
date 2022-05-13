307a58c0f5242f856ddaccd2b936e8a9c4807bb8
puja <pujarana1796@gmail.com>
2022-05-13 12:17:56 +0530

    puja added first commit

--- BinarySearchTree/BST.cs
+++ BinarySearchTree/BST.cs
@@ -6,55 +6,65 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-   
-        public class BST<T> where T : IComparable<T>
+
+    public class BST<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BST<T> LeftTree { get; set; }
+        public BST<T> RightTree { get; set; }
+        public BST(T nodeData)
         {
-            public T NodeData { get; set; }
-            public BST<T> LeftTree { get; set; }
-            public BST<T> RightTree { get; set; }
-            public BST(T nodeData)
-            {
-                this.NodeData = nodeData;
-                this.LeftTree = LeftTree;
-                this.RightTree = RightTree;
-            }
-            int leftCount = 0, rightCount = 0;
-            bool result = false;
-            public void Insert(T item)
-            {
-                T currentNodeValue = this.NodeData;
-                if (currentNodeValue.CompareTo(item) > 0)
-                {
-                    if (this.LeftTree == null)
-                        this.LeftTree = new BST<T>(item);
-                    else
-                        this.LeftTree.Insert(item);
-                }
-                else
-                {
-                    if (this.RightTree == null)
-                        this.RightTree = new BST<T>(item);
-                    else
-                        this.RightTree.Insert(item);
-                }
-            }
-            public void Display()
-            {
-                if (this.LeftTree != null)
-                {
-                    this.leftCount++;
-                    this.LeftTree.Display();
-
-                }
-                Console.WriteLine(this.NodeData.ToString());
-                if (this.RightTree != null)
-                {
-                    this.rightCount++;
-                    this.RightTree.Display();
-                }
-            }
-
+            this.NodeData = nodeData;
+            this.LeftTree = LeftTree;
+            this.RightTree = RightTree;
         }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if (currentNodeValue.CompareTo(item) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BST<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BST<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+
+        public void GetSize()
+        {
+          
+                Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+           
+        }
+
     }
+}
+
+
+    
 
 
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,12 +7,17 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to binary tree search!");
-
             BST<int> bst = new(56);
             bst.Insert(30);
             bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(11);
+            bst.Insert(65);
             bst.Display();
-
+            bst.GetSize();
         }
     }
 }
